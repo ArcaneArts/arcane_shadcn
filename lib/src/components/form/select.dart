edb88525267b25f0fb8c121fd5ebe216b7277c00
cyberpwn <dan@arcane.art>
2025-03-29 09:10:28 -0400

    Fix multiselect issue placeholder gone on empty but not null

--- lib/src/components/form/select.dart
+++ lib/src/components/form/select.dart
@@ -806,7 +806,7 @@ class MultiSelect<T> extends StatelessWidget with SelectBase<Iterable<T>> {
       constraints: constraints,
       popupConstraints: popupConstraints,
       popupWidthConstraint: popupWidthConstraint,
-      value: value,
+      value: value != null && value!.isEmpty ? null : value,
       borderRadius: borderRadius,
       padding: padding,
       popoverAlignment: popoverAlignment,
