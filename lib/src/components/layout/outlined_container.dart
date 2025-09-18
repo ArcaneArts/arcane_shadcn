7fbf0a01c81b2313cac8307003a1fe6e1e679b36
cyberpwn <dan@arcane.art>
2025-09-18 10:33:40 -0500

    x

--- lib/src/components/layout/outlined_container.dart
+++ lib/src/components/layout/outlined_container.dart
@@ -305,7 +305,7 @@ class _OutlinedContainerState extends State<OutlinedContainer> {
           stackFit: StackFit.passthrough,
           padding: EdgeInsets.zero,
           strokeCap: StrokeCap.round,
-          borderPadding: EdgeInsets.all(0.5),
+          borderPadding: EdgeInsets.all(borderWidth),
           child: childWidget);
     }
 
